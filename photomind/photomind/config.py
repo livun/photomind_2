bb8bc236e94596a852a6118022becc09b3adcb52
livun <liv.underhaug@stud.uis.no>
2020-10-07 11:43:23 +0200

    oppdatert

--- photomind/photomind/config.py
+++ photomind/photomind/config.py
@@ -1,15 +1,6 @@
-import os
-
+from datetime import timedelta
 
 class Config:
-    #SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY='5791628bb0b13ce0c676dfde280ba245'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    #MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    EMAIL_USER='photomind2020@gmail.com'
-    #MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    EMAIL_PASS='flask_app_project'
+   SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+   SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+   PERMANENT_SESSION_LIFETIME =  timedelta(minutes=5)
