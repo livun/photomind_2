c8a0891f565e898b2781e7d42adf665408816c37
Åse Sagebakken <aase.sagebakken@hotmail.com>
2020-10-19 13:27:54 +0200

    jkk

--- photomind/photomind/main/routes.py
+++ photomind/photomind/main/routes.py
@@ -10,16 +10,3 @@ def home():
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
-<<<<<<< HEAD
-@main.route("/admin")
-@basic_auth.required
-def admin_view():
-    return render_template('admin.html')
-
-
-#if __name__ == "__main__":
-#    main.run(ssl_context='adhoc')
-
-# flask run --cert=adhoc
-=======
->>>>>>> 5cf015f31d82db9e6d9eae130e999943b4460f54
