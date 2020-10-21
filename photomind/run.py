b6e04b823f8d023a1af92085e18fd50fc5dfc519
Åse Sagebakken <aase.sagebakken@hotmail.com>
2020-10-21 13:21:35 +0200

    run

--- photomind/run.py
+++ photomind/run.py
@@ -1,16 +1,8 @@
 from photomind import create_app
 from flask import Flask
-#from flask_sslify import SSLify
-#import os
-
-#app = Flask(__name__)
-#app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
-
-#sslify = SSLify(app)
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-    #app.run(ssl_context=('cert.pem', 'key.pem'))
