0c04de8630ac3fabe90d549ef1765e282453b7c2
minawoien <mc.woien@stud.uis.no>
2020-10-22 21:41:23 +0200

    commit

--- photomind/photomind/posts/forms.py
+++ photomind/photomind/posts/forms.py
@@ -1,11 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError
+from photomind.models import Post
+
+avoid = ['#', '$', '%', '/', '(', ')', '=', '{', '}', '[', ']', ':', ';', '\\', '*', '@', '^', '¨', '~', '§']
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
 
+    def validate_content(self, content):
+        content = content.data
+        for i in avoid:
+            if i in content:
+                raise ValidationError('Special characters is not allowed, try again.')
 
-                                    
+    def validate_title(self, title):
+        title = title.data
+        for i in avoid:
+            if i in title:
+                raise ValidationError('Special characters is not allowed, try again.')
--- photomind/photomind/users/forms.py
+++ photomind/photomind/users/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from flask_login import current_user
 from photomind.models import User
 
+avoid = ['#', '$', '%', '/', '(', ')', '=', '{', '}', '[', ']', ':', ';', '\\', '*', '^', '¨', '~', '§']
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -15,7 +16,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     question = SelectField('Questions', choices = ["What was the house number and street name you lived in as a child?",
-            "    “What is your oldest cousin’s first name?”?",
+            "What is your oldest cousin’s first name?",
             "What primary school did you attend?", "In what town or city was your first full time job?",
             "What is the middle name of your oldest child?"], validators=[DataRequired()])
     answer = StringField('Answer', validators=[DataRequired()])
@@ -25,12 +26,29 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
+        for i in avoid:
+            if i in username.data:
+                raise ValidationError('Special characters is not allowed, try again.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
+        for i in avoid:
+            if i in email.data:
+                raise ValidationError('Special characters is not allowed, try again.')
 
+    def validate_password(self, password):
+        password = password.data
+        for i in avoid:
+            if i in password:
+                raise ValidationError('Special characters is not allowed, try again.')
+
+    def validate_answer(self, answer):
+        answer = answer.data
+        for i in avoid:
+            if i in answer:
+                raise ValidationError('Special characters is not allowed, try again.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -39,7 +57,16 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+    def validate_email(self, email):
+        for i in avoid:
+            if i in email.data:
+                raise ValidationError('Special characters is not allowed, try again.')
 
+    def validate_password(self, password):
+        password = password.data
+        for i in avoid:
+            if i in password:
+                raise ValidationError('Special characters is not allowed, try again.')
 
 
 class UpdateAccountForm(FlaskForm):
@@ -51,15 +78,14 @@ class UpdateAccountForm(FlaskForm):
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
     submit = SubmitField('Update')
 
-
-
-
-
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. Please choose a different one.')
+        for i in avoid:
+            if i in username.data:
+                raise ValidationError('Special characters is not allowed, try again.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
@@ -68,16 +94,34 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That email is taken. Please choose a different one.')
 
 
+
+
 class NewPasswordForm(FlaskForm):
     email = StringField('Email',
                     validators=[DataRequired(), Email()])
     question = SelectField('Questions', choices = ["What was the house number and street name you lived in as a child?",
-                "What were the last four digits of your childhood telephone number?",
-                "What primary school did you attend?", "In what town or city was your first full time job?",
-                "What is the middle name of your oldest child?"], validators=[DataRequired()])
+            "What is your oldest cousin’s first name?",
+            "What primary school did you attend?", "In what town or city was your first full time job?",
+            "What is the middle name of your oldest child?"], validators=[DataRequired()])
     answer = StringField('Aswer', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=16, max=64)])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
 
+    def validate_email(self, email):
+        for i in avoid:
+            if i in email.data:
+                raise ValidationError('Special characters is not allowed, try again.')
+
+    def validate_password(self, password):
+        password = password.data
+        for i in avoid:
+            if i in password:
+                raise ValidationError('Special characters is not allowed, try again.')
+
+    def validate_answer(self, answer):
+        answer = answer.data
+        for i in avoid:
+            if i in answer:
+                raise ValidationError('Special characters is not allowed, try again.')
