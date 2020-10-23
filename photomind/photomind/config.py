a5cae0c226e6360f90616751d952e63b0860f220
minawoien <mc.woien@stud.uis.no>
2020-10-23 11:53:12 +0200

    hei

--- photomind/photomind/config.py
+++ photomind/photomind/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-   SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+   SQLALCHEMY_DATABASE_URI = os.environ['postgres://qexxhdcednqaso:cee7a086a79c6b4527018bdc220b1a26735a153d7c3fb8d2efc8eaaafdef0796@ec2-34-232-24-202.compute-1.amazonaws.com:5432/dc38f79c09ptv']
    PERMANENT_SESSION_LIFETIME =  timedelta(minutes=20)
    
    MAX_CONTENT_LENGTH = 1024 * 1024  # 1MB max-limit
