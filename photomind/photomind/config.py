a561a022280ee93365108c9215e34628b6bffc16
Åse Sagebakken <aase.sagebakken@hotmail.com>
2020-10-25 10:32:38 +0100

    endring

--- photomind/photomind/config.py
+++ photomind/photomind/config.py
@@ -4,7 +4,8 @@ import os
 class Config:
    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
 
-   SQLALCHEMY_DATABASE_URI = os.environ['postgres://qexxhdcednqaso:cee7a086a79c6b4527018bdc220b1a26735a153d7c3fb8d2efc8eaaafdef0796@ec2-34-232-24-202.compute-1.amazonaws.com:5432/dc38f79c09ptv']
+   SQLALCHEMY_DATABASE_URI = 'postgres://qexxhdcednqaso:cee7a086a79c6b4527018bdc220b1a26735a153d7c3fb8d2efc8eaaafdef0796@ec2-34-232-24-202.compute-1.amazonaws.com:5432/dc38f79c09ptv'
+
    
    PERMANENT_SESSION_LIFETIME =  timedelta(minutes=20)
    
